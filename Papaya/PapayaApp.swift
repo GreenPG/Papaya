785eeaa6d3bbc09bd41a12c47d9e937942be9ac0
Gillian <pasquettegillian@sfr.fr>
2026-06-01 13:39:26 +0200

    Initial Commit

---
+++ Papaya/PapayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapayaApp.swift
+//  Papaya
+//
+//  Created by Apprenant76 on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PapayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
