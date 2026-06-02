d20d3c080a8775426b4153097f7610b37eb3d962
Gillian <pasquettegillian@sfr.fr>
2026-06-02 15:08:43 +0200

    add *.xcodeproj to .gitignore

---
+++ Papaya/TutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialView.swift
+//  Papaya
+//
+//  Created by Apprenant76 on 02/06/2026.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialView()
+}
