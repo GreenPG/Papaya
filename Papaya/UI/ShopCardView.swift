85e6ce17f3f85057349f23df52991ef7db07dbb5
Adarem0 <a.c.lozano@outlook.fr>
2026-06-03 15:23:01 +0200

    Add user Model

---
+++ Papaya/UI/ShopCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ShopCardView.swift
+//  Papaya
+//
+//  Created by Apprenant 103 on 02/06/2026.
+//
+
+import SwiftUI
+
+struct ShopCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShopCardView()
+}
