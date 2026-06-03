5ac34e13197863f04e1af9759006b9678228645d
Gillian <pasquettegillian@sfr.fr>
2026-06-03 14:14:28 +0200

    Add Background component

---
+++ Papaya/UI/Background.swift
@@ -0,0 +1,21 @@
+//
+//  Background.swift
+//  Papaya
+//
+//  Created by Apprenant76 on 03/06/2026.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    
+    var body: some View {
+        LinearGradient(stops: [Gradient.Stop(color:.white, location:0.4),  Gradient.Stop(color:.greenPapaya.opacity(0.45), location:1)], startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea(.all)
+
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
