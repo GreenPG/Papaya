8caff06a9238b44b2a868d395998b2e6a3784f15
Melissa <meli.tournerie@gmail.com>
2026-06-04 09:01:32 +0200

    add commentModel et messagerie

---
+++ Papaya/messagerie.swift
@@ -0,0 +1,25 @@
+//
+//  messagerie.swift
+//  Papaya
+//
+//  Created by Apprenant 72 on 03/06/2026.
+//
+
+import SwiftUI
+
+struct messagerie: View {
+    var body: some View {
+        VStack{
+            VStack {
+//                let sender = User(userName: "Mister Courgette", userID: 2, userMembershipSince: <#Date?#>,userAdress: "blabla", userStarRating: 5, userEverySell: 12)
+                ZStack{
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    messagerie()
+}
