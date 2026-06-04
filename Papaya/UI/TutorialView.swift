2369553f1b628e68f80cc342f6a7f883c9d2b825
Gillian <pasquettegillian@sfr.fr>
2026-06-04 15:04:39 +0200

    Move TutorialView in UI folder

---
+++ Papaya/UI/TutorialView.swift
@@ -0,0 +1,54 @@
+//
+//  TutorialView.swift
+//  Papaya
+//
+//  Created by Apprenant76 on 02/06/2026.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+        
+    var tutorial: Tutorial
+    var body: some View {
+        ZStack {
+            BackgroundView()
+            ScrollView {
+                HStack {
+                    Text(tutorial.name)
+                        .font(
+                            .custom(
+                                "Courgette",
+                                size: 40,
+                                relativeTo: .title
+                            )
+                        )
+                        .multilineTextAlignment(.leading)
+                        .lineSpacing(10)
+                    Spacer()
+                }
+                .padding(.bottom, 15)
+                HStack {
+                    Text(tutorial.intro)
+                        .multilineTextAlignment(.leading)
+                    Spacer()
+                }
+                ForEach(
+                    Array(tutorial.tutorialSections.enumerated()),
+                    id: \.offset
+                ) { index, tutorialSection in
+                    TutorialSectionView(
+                        index: index + 1,
+                        tutorialSection: tutorialSection
+                    )
+                }
+            }
+            .padding(.horizontal, 25)
+        }
+    }
+}
+
+
+#Preview {
+    TutorialView(tutorial: tutorials[0])
+}
