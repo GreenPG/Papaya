3961c4a1b0791b8c105fa86265a6c542836205f3
Melissa <meli.tournerie@gmail.com>
2026-06-05 14:27:17 +0200

    Modele message et modele bubleMessage

---
+++ Papaya/Models/BubbleMessageExtractedView.swift
@@ -0,0 +1,40 @@
+//
+//  BubbleMessage.swift
+//  Papaya
+//
+//  Created by Apprenant 72 on 04/06/2026.
+//
+
+import SwiftUI
+
+struct BubbleMessage: View {
+    var message: Message
+    var body: some View {
+        VStack (alignment: message.isUser ? .trailing : .leading) {
+            ZStack(alignment: message.isUser ? .trailing : .leading){
+                UnevenRoundedRectangle(
+                    topLeadingRadius:20,
+                    bottomLeadingRadius: 20,
+                    bottomTrailingRadius: 0,
+                    topTrailingRadius: 20
+                )
+                .fill(.beigePapaya.secondary)
+                .stroke(.brownPapaya, lineWidth: 3)
+                
+                
+                Text(message.content)
+                    .multilineTextAlignment(.trailing)
+                    .frame(maxWidth: 200) //permet de limiter la largeur du texte et donc le retour a la ligne (gestion de la longueur de la bulle de texte en modifiant la longueur du container avec .fixedsize
+                    .padding()
+            }
+            .fixedSize(horizontal: false, vertical: true)
+            Text(message.date)
+        }
+//        .ignoresSafeArea => pour les backgrounds
+        .padding(.horizontal, 8) //gestion du paddind au bord de l'ecran
+    }
+}
+
+#Preview {
+    BubbleMessage(message: messages[0])
+}
