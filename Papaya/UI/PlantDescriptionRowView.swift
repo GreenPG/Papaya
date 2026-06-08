3bcef427dbbcdf8099e46701e62801244e0e2807
Gillian <pasquettegillian@sfr.fr>
2026-06-08 14:46:02 +0200

    Add PlantDescriptionRowView

--- Papaya/UI/PlantDescriptionRowView.swift
+++ Papaya/UI/PlantDescriptionRowView.swift
@@ -1,6 +1,31 @@
-       Image(systemName: "chart.line.uptrend.xyaxis")
-                            .font(.system(size: 35))
-                            .foregroundStyle(.greenPapaya)
-                            .frame(minWidth: 50, alignment: .center)
-                        Text(plant.yield)
-                        Spacer()
+//
+//  PlantDescriptionRowView.swift
+//  Papaya
+//
+//  Created by Apprenant76 on 08/06/2026.
+//
+
+import SwiftUI
+
+struct PlantDescriptionRowView: View {
+
+    var plant: PlantModel
+    var image: String
+    var color: Color
+    var text: String
+
+    var body: some View {
+        HStack(spacing: 10) {
+            Image(systemName: image)
+                .font(.system(size: 35))
+                .foregroundStyle(color)
+                .frame(minWidth: 50, alignment: .center)
+            Text(text)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    PlantDescriptionRowView(plant: tomato, image: "sun.max", color: Color.yellow, text: "3-6kg/saison")
+}
