9290463456daacc055929b3d1510a5a6b226a0ed
Gillian <pasquettegillian@sfr.fr>
2026-06-12 16:22:35 +0200

    fix User model with missing userProfilePicture attribute

--- Papaya/Models/userModel.swift
+++ Papaya/Models/userModel.swift
@@ -10,6 +10,7 @@ import Foundation
 struct User: Identifiable {
     let id = UUID()
     let userName: String
+    let userProfilePicture: String
     let userAdress: String
     let userStarRating: Float
     let userEverySell: Int
