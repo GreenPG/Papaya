7f7c2b930e053eba9b6e2059fb8f255d2070edec
Adarem0 <a.c.lozano@outlook.fr>
2026-06-12 18:36:06 +0200

    add UserModel

---
+++ Papaya/Models/UserModel.swift
@@ -0,0 +1,17 @@
+//
+//  userModel.swift
+//  Papaya
+//
+//  Created by Apprenant 103 on 03/06/2026.
+//
+
+import Foundation
+
+struct User {
+    let userName: String
+    let userID: Int
+    let userMembershipSince: Date?
+    let userAdress: String
+    let userStarRating: Float
+    let userEverySell: Int
+}
