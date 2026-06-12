f566de930711cff04daf10d50664e9797bca0a58
Adarem0 <a.c.lozano@outlook.fr>
2026-06-12 18:36:38 +0200

    resolve conflict

--- Papaya/messagerie.swift
+++ Papaya/messagerie.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct messagerie: View {
+    //faire une copie du array messages pour pouvoir appliquer un state et un append pour mettre a jour la conv
+    
+    @State private var comment: String = ""
+    @State private var validComment: [String ]= [""]
+//    var user: User
     var body: some View {
-        VStack{
-            VStack {
-//                let sender = User(userName: "Mister Courgette", userID: 2, userMembershipSince: <#Date?#>,userAdress: "blabla", userStarRating: 5, userEverySell: 12)
-                ZStack{
+        VStack {
+            VStack /*(.alignment.center)*/{
+//                Image()
+//                ZStack{}
+                //recup le profil + mise forme
+            }
+
+            VStack{
+                //recup message
+                ForEach(messages) {message in
+                    BubbleMessage(message: message)
+                }
+//
+            }
+            
+            HStack {
+                TextField("Saisissez votre commentaire", text: $comment) // Var bindée car il y a de l'affichage et la modif
+                Button{
+                    validComment = $comment
                     
+                    
+                    comment = ""
+                }label: {
+                    Image(systemName: "paperplane.fill")
                 }
             }
         }
