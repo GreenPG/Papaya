7d018869af4780f702af00792a8caeaed933037f
Aymerick Benares <tekurescontact@gmail.com>
2026-06-12 10:15:23 +0200

    Search bar works & articles

---
+++ Papaya/UI/ArticleDetailView.swift
@@ -0,0 +1,56 @@
+//
+//  ArticleDetailView.swift
+//  Papaya
+//
+//  Created by Apprenant 83 on 11/06/2026.
+//
+
+import SwiftUI
+
+struct ArticleDetailView: View {
+    
+    let article: Article
+    
+    var body: some View {
+        
+        ZStack{
+            BackgroundView()
+                .ignoresSafeArea()
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    Image(article.imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 250)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding(.horizontal, -15)
+                    
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(article.name)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Divider()
+                        
+                        Text(article.description)
+                            .font(.body)
+                            .foregroundColor(.primary)
+                            .lineSpacing(7)
+                        
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.top)
+                
+            }
+            
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    ArticleDetailView(article: articles[4])
+}
