78fa9d57d1e5e676bd8bb6aee5745049e6feed68
Gillian <pasquettegillian@sfr.fr>
2026-06-13 19:38:54 +0200

    change AddModel image attribute from String to Image

--- Papaya/Models/InfoPostShopModel.swift
+++ Papaya/Models/InfoPostShopModel.swift
@@ -5,22 +5,20 @@
 //  Created by Apprenant 72 on 13/06/2026.
 //
 
-import Foundation
-
-import Foundation
+import SwiftUI
 
 struct AddModel: Identifiable {
 let id = UUID()
     let title: String
     let description: String
-    let image: String
+    let image: Image
     let tag: String? = nil
     var isUserInfoVisible: Bool? = false
     }
 
 let addModels: [AddModel] = [
-    AddModel(title: "Graines de courge", description: "Lorem ipsum at quid tincidunt.", image: "graines-de-courge"),
-    AddModel(title: "Graines de passiflore", description: "Lorem ipsum at quid tincidunt.", image: "passiflore"),
-    AddModel(title: "Graines de capucine", description: "Lorem ipsum at quid tincidunt.", image: "capucine"),
-    AddModel(title: "Graines de tomates", description: "Lorem ipsum at quid tincidunt", image: "graines-de-tomate"),
+    AddModel(title: "Graines de courge", description: "Lorem ipsum at quid tincidunt.", image: Image("graines-de-courge")),
+    AddModel(title: "Graines de passiflore", description: "Lorem ipsum at quid tincidunt.", image: Image("passiflore")),
+    AddModel(title: "Graines de capucine", description: "Lorem ipsum at quid tincidunt.", image: Image("capucine")),
+    AddModel(title: "Graines de tomates", description: "Lorem ipsum at quid tincidunt", image: Image("graines-de-tomate")),
 ]
---
+++ Papaya/UI/AdView.swift
@@ -0,0 +1,18 @@
+//
+//  AdView.swift
+//  Papaya
+//
+//  Created by Apprenant76 on 13/06/2026.
+//
+
+import SwiftUI
+
+struct AdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdView()
+}
